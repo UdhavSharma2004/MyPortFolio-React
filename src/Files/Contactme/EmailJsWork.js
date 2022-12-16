9f98f59fb363d41716405b4638f49e9b652192bb
Udhav Sharma <96074679+UdhavSharma2004@users.noreply.github.com>
2022-12-16 12:42:15 +0530

    final changes

--- src/Files/Contactme/EmailJsWork.js
+++ src/Files/Contactme/EmailJsWork.js
@@ -1,33 +1,53 @@
 import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import { toast } from "react-toastify";
-export default function EmailJS(){
+export default function EmailJS() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_1aryue6', 'template_n3w5q8g', form.current, 'dUB8BLFueY5Hlpfa2')
-      .then((result) => {
+    let nameval = document.getElementById("NameSend").value;
+    let emailval = document.getElementById("EmailSend").value;
+    let messval = document.getElementById("MessageSend").value;
+    if (nameval !== "" && emailval !== "" && messval !== "") {
+      emailjs.sendForm('service_1aryue6', 'template_n3w5q8g', form.current, 'dUB8BLFueY5Hlpfa2')
+        .then((result) => {
           console.log(result.text);
           toast.success("Mail sent successfully !");
-      }, (error) => {
+        }, (error) => {
           console.log(error.text);
-      });
+        });
+      document.getElementById("NameSend").value = "";
+      document.getElementById("EmailSend").value = "";
+      document.getElementById("MessageSend").value = "";
+    }
+    else {
+      toast.error("Something is empty 😐 !!");
+    }
   };
   // const sendEmailTest = (e) => {
   //   e.preventDefault();
-
-  //   toast.success("Mail sent successfully !");
+  //   let nameval=document.getElementById("NameSend").value;
+  //   let emailval=document.getElementById("EmailSend").value;
+  //   let messval=document.getElementById("MessageSend").value;
+  //   if(nameval!==""&&emailval!==""&&messval!==""){
+  //     toast.success("Mail sent successfully !!");
+  //     document.getElementById("NameSend").value="";
+  //     document.getElementById("EmailSend").value="";
+  //     document.getElementById("MessageSend").value="";
+  // }
+  // else{
+  //     toast.error("Something is empty 😐 !!");
+  //   }
   // };
 
   return (
     <form ref={form} onSubmit={sendEmail}>
       <h3 className="text-twblue text-[2.5rem] font-black text-center mb-2">Direct Contact</h3>
-      <input type="text" placeholder="YOUR NAME" name="user_name" className="bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[75px]  m-2 placeholder-twblue p-2" />
-      <input type="email" placeholder="YOUR EMAIL" name="user_email" className="bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[75px]  m-2 placeholder-twblue p-2" />
-      <textarea name="message" placeholder="ENTER MESSAGE" className=" bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[200px] m-2 placeholder-twblue p-2" />
-      <input type="submit" value="Send" className=" bg-transparent border-[3px] border-white rounded-[10px]  w-[75px] h-[50px] my-[20px] mx-[40%] active:bg-twblue active:text-white duration-[100ms] delay-[100ms]"/>
+      <input id="NameSend" type="text" placeholder="YOUR NAME" name="user_name" className="bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[75px]  m-2 placeholder-twblue p-2 toEmpty" />
+      <input id="EmailSend" type="email" placeholder="YOUR EMAIL" name="user_email" className="bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[75px]  m-2 placeholder-twblue p-2 toEmpty" />
+      <textarea id="MessageSend" name="message" placeholder="ENTER MESSAGE" className=" bg-transparent border-[3px] border-white rounded-[30px] w-[100%] h-[200px] m-2 placeholder-twblue p-2 toEmpty" />
+      <input type="submit" value="Send" className=" bg-transparent border-[3px] border-white rounded-[10px]  w-[75px] h-[50px] my-[20px] mx-[40%] active:bg-twblue active:text-white duration-[100ms] delay-[100ms]" />
     </form>
   );
 };
